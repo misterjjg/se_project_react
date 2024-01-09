d5938a8914c2652f88d31c626238329b1f7904c1
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-01-08 23:06:24 -0800

    profile.js updated

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,14 +2,22 @@ import "../Profile/Profile.css";
 import SideBar from "../SideBar/SideBar";
 import ClothesSection from "../ClothesSection/ClothesSection";
 
-function Profile({ clothingItems, onSelectCard, onCreateModal }) {
+function Profile({
+  clothingItems,
+  onSelectCard,
+  onCreateModal,
+  onEditProfileModal,
+  onLogout,
+  onCardLike,
+}) {
   return (
     <main className="profile">
-      <SideBar />
+      <SideBar onEditProfileModal={onEditProfileModal} onLogout={onLogout} />
       <ClothesSection
         clothingItems={clothingItems}
         onSelectCard={onSelectCard}
         onCreateModal={onCreateModal}
+        onCardLike={onCardLike}
       />
     </main>
   );
