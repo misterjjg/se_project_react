bcbf9a7e72f2fcd9c1390e6757a43094f06ae845
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-01-08 23:38:05 -0800

    added like button to item card

--- src/components/ItemCard/ItemCard.js
+++ src/components/ItemCard/ItemCard.js
@@ -1,6 +1,20 @@
 import "./ItemCard.css";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import React, { useContext } from "react";
 
-const ItemCard = ({ item, onSelectCard }) => {
+const ItemCard = ({ item, onSelectCard, onCardLike }) => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const isLiked = item.likes.some((_id) => _id === currentUser._id);
+
+  const itemLikeButtonClassName = `card__likeButton ${
+    isLiked ? "card__likeButton-active" : "card__likeButton-inactive"
+  }`;
+
+  const handleCardLikeClick = (item) => {
+    onCardLike(item, isLiked, currentUser);
+  };
+
   return (
     <div className="card__item">
       <div>
@@ -11,7 +25,14 @@ const ItemCard = ({ item, onSelectCard }) => {
           onClick={() => onSelectCard(item)}
         />
       </div>
-      <div className="card__name">{item.name}</div>
+      <div className="card__title">
+        <h2 className="card__name">{item.name}</h2>
+        <button
+          className={itemLikeButtonClassName}
+          type="button"
+          onClick={() => handleCardLikeClick(item)}
+        ></button>
+      </div>
     </div>
   );
 };
