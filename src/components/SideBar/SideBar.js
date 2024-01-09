15fddd10769a464861088123406c359c873c3851
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-01-08 22:54:44 -0800

    sidebar js/css updated to have buttons/currentuser

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,13 +1,41 @@
 import "./SideBar.css";
-import avatar from "./../../images/avatar.svg";
+// import avatar from "./../../images/avatar.svg";
+import React, { useContext } from "react";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
 
-const SideBar = () => {
+const SideBar = ({ onEditProfileModal, onLogout }) => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const handleLogoutClick = () => {
+    onLogout();
+  };
+
   return (
     <div className="sidebar">
       <div>
-        <img className="sidebar__avatar" src={avatar} alt="Avatar Logo" />
+        <img
+          className="sidebar__avatar"
+          src={currentUser.avatar}
+          alt="Avatar Logo"
+        />
       </div>
-      <p className="sidebar__name">Terrence Tegegne</p>
+      <p className="sidebar__name">{currentUser.name}</p>
+      <div className="sidebar__button-container">
+        <button
+          className="sidebar__button"
+          type="button"
+          onClick={onEditProfileModal}
+        >
+          Change Profile Data
+        </button>
+        <button
+          className="sidebar__button"
+          type="button"
+          onClick={handleLogoutClick}
+        >
+          Log Out
+        </button>
+      </div>
     </div>
   );
 };
