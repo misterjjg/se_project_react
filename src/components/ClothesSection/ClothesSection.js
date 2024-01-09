307656e32f5481ebb80a3c1e4da3a57c33674e29
Joshua Gonzalez <misterjjgonzabusi@gmail.com>
2024-01-08 23:03:14 -0800

    clothes section updated for current user

--- src/components/ClothesSection/ClothesSection.js
+++ src/components/ClothesSection/ClothesSection.js
@@ -1,22 +1,41 @@
 import "./ClothesSection.css";
 import ItemCard from "../ItemCard/ItemCard";
+import { CurrentUserContext } from "../../contexts/CurrentUserContext";
+import { useContext } from "react";
 
-const ClothesSection = ({ clothingItems, onSelectCard, onCreateModal }) => {
+const ClothesSection = ({
+  clothingItems,
+  onSelectCard,
+  onCreateModal,
+  onCardLike,
+}) => {
+  const currentUser = useContext(CurrentUserContext);
+
+  const ownedItems = clothingItems.filter((item) => {
+    return item.owner === currentUser._id;
+  });
+
   return (
     <section className="clothes-section">
       <div className="clothes-section__container">
-        <h3 className="clothes-section__title">Your Items</h3>
+        <h3 className="clothes-section__title">Your Items:</h3>
         <button
           className="clothes-section__button"
           type="button"
           onClick={onCreateModal}
         >
-          + Add New
+          + Add New Items
         </button>
       </div>
       <ul className="clothes-section__cards">
-        {clothingItems.map((item) => (
-          <ItemCard key={item._id} item={item} onSelectCard={onSelectCard} />
+        {""}
+        {ownedItems.map((item) => (
+          <ItemCard
+            key={item.id || item._id}
+            item={item}
+            onSelectCard={onSelectCard}
+            onCardLike={onCardLike}
+          />
         ))}
       </ul>
     </section>
